de99a555443a928bdd9727afb6241b849f0a695f
mustafa <moshassen000@gmail.com>
2024-08-20 17:00:09 +0300

    theme and custom appBar

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
