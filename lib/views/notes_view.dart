bf2894af41fe842f7d8a4a112c769fc5f1b217da
mustafa <moshassen000@gmail.com>
2024-11-23 22:18:37 +0200

    create notes cubit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/constant.dart';
+import 'package:note_app/cubit/cubit/notes_cubit.dart';
 import 'package:note_app/views/widgets/botton_sheet.dart';
 import 'package:note_app/views/widgets/notes_view_body.dart';
 
@@ -8,21 +10,24 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: kPrimaryColor,
-        foregroundColor: Colors.black,
-        onPressed: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              context: context,
-              builder: (context) {
-                return const AddNoteBottomSheet();
-              });
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: kPrimaryColor,
+          foregroundColor: Colors.black,
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheet();
+                });
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
